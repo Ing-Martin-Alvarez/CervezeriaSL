44b1a1570bdc1f0aaa646966e0264c1fc222f419
Martín Álvarez Salazar <sistemas.martin.alvarez@gmail.com>
2024-08-06 22:42:50 -0600

    Add files via upload

---
+++ CervezeriaSL/src/cervezeriasl/CervezeriaSL.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cervezeriasl;
+
+/**
+ *
+ * @author EvilDeath
+ */
+public class CervezeriaSL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Venta ven=new Venta();
+        ven.setVisible(true);
+        ven.setLocationRelativeTo(null);
+    }
+    
+}
